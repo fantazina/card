ebc33a3efdfe6a42b30a6e3ea61d4172a8a098d6
fantazina <137284904+fantazina@users.noreply.github.com>
2024-03-05 23:22:16 +0900

    새로운과제

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Array from './component/Array';
 import AddList from './component/AddList';
 import AddList2 from './component/AddList2';
 import Contents from './component/Contents';
+import AddList3 from './component/AddList3';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       {/* <Array /> */}
       {/* <AddList /> */}
       {/* <AddList2 /> */}
+      {/* <AddList3 /> */}
       <Contents />
     </div>
   );
---
+++ src/component/AddList3.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import style from './css/Card.module.css';
+
+const AddList3 = () => {
+    const[input, setInput] = useState({
+        name : '',
+        age : ''
+    })
+    const[arrList, setArrList] = useState([])
+    const[search, setSearch] = useState('')
+    const[searchAge, setSearchAge] = useState()
+
+    const onInput = (e) => {
+        const { name, value } = e.target
+        setInput({
+            ...input,
+            [name] : value
+        })
+    }
+
+    const addItem = () => {
+        if(input.name !== '' && input.age !== '') {
+            setArrList([...arrList, input])
+            setInput({
+                name : '',
+                age : ''
+            })
+        }
+    }
+
+    const onDelete = (i) => {
+        const updateItem = arrList.filter((item, index) => index !== i)
+        setArrList(updateItem)
+    }
+
+    const searchIn = (e) => {
+        const { value } = e.target
+        setSearch(value)
+    }
+
+    const onSearch = () => {
+        const searchAge = arrList.find(item => item.name === search).age
+        setSearchAge(searchAge)
+
+    }
+
+    return (
+        <div>
+            <div className={ style.inputListDiv }>
+                <p>Name : </p> 
+                <input onChange={ onInput } name='name' value={ input.name }/>
+                <p>Age : </p> 
+                <input onChange={ onInput } name='age' value={ input.age }/> 
+                <button onClick={ addItem }>추가</button>
+            </div> 
+
+            <div className={ style.inputDiv }>
+                {
+                    arrList.map((item, index) => 
+                        <div className={ style.inputItem }>
+                            <div className={ style.inputItemName }>
+                                <p>이름 : { item.name }</p>
+                                <p style={{ marginLeft : '30px' }}>나이 : { item.age }</p>
+                                <button onClick={ () => onDelete(index) }>삭제</button>
+                            </div>
+                        </div>
+                    )
+                }
+                
+
+            </div>
+
+            <div className={ style.searchList }>
+                <div className={ style.searchDiv }>
+                    <input onChange={ searchIn } value={ search }/>
+                    <button onClick={ onSearch }>찾기</button>
+                </div>    
+
+                <div className={ style.searchAgeDiv }>     
+                    {   
+                        searchAge && (
+
+                        
+                            <div className={ style.searchAge }>
+                                <p>나이 : { searchAge } </p>
+                            </div>
+                        )
+                    }            
+                        
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default AddList3;
